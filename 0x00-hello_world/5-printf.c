6ca10ef2da9bac84dd882c4b282437c8f14b8044
Anon2356 <amanonymous579@gmail.com>
2022-04-08 21:32:13 +0300

    5th task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry point 
+ * main - entry point
  * My hello, printf task
- * Return: return 1 after program execution 
+ * Return: return 1 after program execution
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
